4950fbee83b7eb31ceef28e15b623d6859f9f2bc
DiogoSantoss <diogosantoss360@hotmail.com>
2020-12-17 21:26:30 +0000

    Added tests files and scripts

---
+++ tests/prj-ei-daily-202010291254/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CLASSPATH=../../po-uilib/po-uilib.jar:../../woo-core/woo-core.jar:../woo-app.jar
+CLASSPATH=/usr/share/java/po-uuilib.jar:/mnt/c/Users/Diogo\ Santos/Desktop/project/woo-core/src:/mnt/c/Users/Diogo\ Santos/Desktop/project/woo-app/src
+
+for x in auto-tests/*.in; do
+    if [ -e $x:r.import ]; then
+        java -cp $CLASSPATH -Dimport=$x:r.import -Din=$x -Dout=$x:r.outhyp woo.app.App
+    else
+        java -cp $CLASSPATH                      -Din=$x -Dout=$x:r.outhyp woo.app.App
+    fi
+
+    diff -cBb -w $x:r.out $x:r.outhyp > $x:r.diff
+    if [ -s $x:r.diff ]; then
+        echo "FAIL: $x. See file $x:r.diff"
+    else
+        echo -n "."
+        rm -f $x:r.diff $x:r.outhyp 
+    fi
+done
+
+rm -f saved*
+
+echo "Done."
+
